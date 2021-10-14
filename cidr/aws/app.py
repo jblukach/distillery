9e3b61a2644b1542c7ed47d80c033e736e719a21
John Lukach <84751182+jblukach@users.noreply.github.com>
2021-10-14 01:37:27 +0000

    aws

---
+++ cidr/aws/app.py
@@ -0,0 +1,79 @@
+import boto3
+import ipaddress
+import json
+import logging
+import os
+import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambdaHandler(event, context):
+    
+    logger.info(event)
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    
+    client = boto3.client('ssm')
+    response = client.get_parameter(Name=os.environ['SSM_PARAMETER'])
+    prevtoken = response['Parameter']['Value']
+    
+    r = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json')
+    logger.info('Download Status Code: '+str(r.status_code))
+    
+    if r.status_code == 200:
+        output = r.json()
+        if prevtoken != output['syncToken']:
+            logger.info('Updating AWS IP Ranges')
+            for cidr in output['prefixes']:
+                sortkey = 'AWS#'+cidr['service']+'#'+cidr['region']+'#'+cidr['ip_prefix']
+                hostmask = cidr['ip_prefix'].split('/')
+                iptype = ipaddress.ip_address(hostmask[0])
+                nametype = 'IPv'+str(iptype.version)+'#'
+                iprange = cidr['ip_prefix']
+                netrange = ipaddress.IPv4Network(cidr['ip_prefix'])
+                first, last = netrange[0], netrange[-1]
+                firstip = int(ipaddress.IPv4Address(first))
+                lastip = int(ipaddress.IPv4Address(last))
+                table.put_item(Item= {  'pk': nametype,
+                                        'sk': sortkey,
+                                        'service': cidr['service'],
+                                        'region': cidr['region'],
+                                        'cidr': iprange,
+                                        'edge': cidr['network_border_group'],
+                                        'created': output['createDate'],
+                                        'firstip': firstip,
+                                        'lastip': lastip
+                                    } )
+            for cidr in output['ipv6_prefixes']:
+                sortkey = 'AWS#'+cidr['service']+'#'+cidr['region']+'#'+cidr['ipv6_prefix']
+                hostmask = cidr['ipv6_prefix'].split('/')
+                iptype = ipaddress.ip_address(hostmask[0])
+                nametype = 'IPv'+str(iptype.version)+'#'
+                iprange = cidr['ipv6_prefix']
+                netrange = ipaddress.IPv6Network(cidr['ipv6_prefix'])
+                first, last = netrange[0], netrange[-1]
+                firstip = int(ipaddress.IPv6Address(first))
+                lastip = int(ipaddress.IPv6Address(last))
+                table.put_item(Item= {  'pk': nametype,
+                                        'sk': sortkey,
+                                        'service': cidr['service'],
+                                        'region': cidr['region'],
+                                        'cidr': iprange,
+                                        'edge': cidr['network_border_group'],
+                                        'created': output['createDate'],
+                                        'firstip': firstip,
+                                        'lastip': lastip
+                                    } )
+            response = client.put_parameter(Name=os.environ['SSM_PARAMETER'],
+                                            Value=output['syncToken'],
+                                            Type='String',
+                                            Overwrite=True)
+        else:
+            logger.info('No AWS IP Range Updates')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Download AWS IP Ranges')
+    }
--- distillery/distillery_stack.py
+++ distillery/distillery_stack.py
@@ -1,10 +1,13 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    aws_dynamodb as _dynamodb,
+    aws_events as _events,
+    aws_events_targets as _targets,
+    aws_iam as _iam,
+    aws_lambda as _lambda,
+    aws_logs as _logs,
+    aws_ssm as _ssm,
+    core as cdk
+)
 
 
 class DistilleryStack(cdk.Stack):
@@ -12,4 +15,114 @@ class DistilleryStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        table = _dynamodb.Table(
+            self, 'cidr',
+            partition_key = {
+                'name': 'pk',
+                'type': _dynamodb.AttributeType.STRING
+            },
+            sort_key = {
+                'name': 'sk',
+                'type': _dynamodb.AttributeType.STRING
+            },
+            billing_mode = _dynamodb.BillingMode.PAY_PER_REQUEST,
+            point_in_time_recovery = True,
+            removal_policy = cdk.RemovalPolicy.DESTROY
+        )
+        
+        table.add_global_secondary_index(
+            index_name = 'firstip',
+            partition_key = {
+                'name': 'pk',
+                'type': _dynamodb.AttributeType.STRING
+            },
+            sort_key = {
+                'name': 'firstip',
+                'type': _dynamodb.AttributeType.NUMBER
+            },
+            projection_type = _dynamodb.ProjectionType.INCLUDE,
+            non_key_attributes = ['created']
+        )
+
+        table.add_global_secondary_index(
+            index_name = 'lastip',
+            partition_key = {
+                'name': 'pk',
+                'type': _dynamodb.AttributeType.STRING
+            },
+            sort_key = {
+                'name': 'lastip',
+                'type': _dynamodb.AttributeType.NUMBER
+            },
+            projection_type = _dynamodb.ProjectionType.INCLUDE,
+            non_key_attributes = ['created']
+        )
+
+        role = _iam.Role(
+            self, 'role',
+            assumed_by = _iam.ServicePrincipal(
+                'lambda.amazonaws.com'
+            )
+        )
+        
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AWSLambdaBasicExecutionRole'
+            )
+        )
+        
+        role.add_to_policy(
+            _iam.PolicyStatement(
+                actions = [
+                    'dynamodb:PutItem',
+                    'ssm:GetParameter',
+                    'ssm:PutParameter',
+                ],
+                resources = [
+                    '*'
+                ]
+            )
+        )
+
+### AWS ###
+
+        awstracker = _ssm.StringParameter(
+            self, 'awstracker',
+            description = 'AWS Distillery Tracker',
+            parameter_name = '/distillery/tracker/aws',
+            string_value = 'EMPTY',
+            tier = _ssm.ParameterTier.STANDARD,
+        )
+
+        awscompute = _lambda.DockerImageFunction(
+            self, 'awscompute',
+            code = _lambda.DockerImageCode.from_image_asset('cidr/aws'),
+            timeout = cdk.Duration.seconds(900),
+            role = role,
+            environment = dict(
+                DYNAMODB_TABLE = table.table_name,
+                SSM_PARAMETER = awstracker.parameter_name
+            ),
+            memory_size = 128,
+        )
+
+        awslogs = _logs.LogGroup(
+            self, 'awslogs',
+            log_group_name = '/aws/lambda/'+awscompute.function_name,
+            retention = _logs.RetentionDays.ONE_DAY,
+            removal_policy = cdk.RemovalPolicy.DESTROY
+        )
+
+        awsevent = _events.Rule(
+            self, 'awsevent',
+            schedule=_events.Schedule.cron(
+                minute='0',
+                hour='*',
+                month='*',
+                week_day='*',
+                year='*'
+            )
+        )
+        awsevent.add_target(_targets.LambdaFunction(awscompute))
+
+###
