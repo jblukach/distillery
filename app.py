9da5c7bd3051eaa06c503d2c4917fc8f8a59e8a5
John Lukach <hello@lukach.io>
2024-01-08 01:26:17 +0000

    rebuild

--- app.py
+++ app.py
@@ -3,15 +3,159 @@ import os
 
 import aws_cdk as cdk
 
+from distillery.distillery_amazon import DistilleryAmazon
+from distillery.distillery_azure import DistilleryAzure
+from distillery.distillery_cloudflare import DistilleryCloudflare
+from distillery.distillery_digitalocean import DistilleryDigitalOcean
+from distillery.distillery_google import DistilleryGoogle
+from distillery.distillery_googlecloud import DistilleryGoogleCloud
+from distillery.distillery_netspi import DistilleryNetSpi
+from distillery.distillery_o365 import DistilleryO365
+from distillery.distillery_okta import DistilleryOkta
+from distillery.distillery_oracle import DistilleryOracle
 from distillery.distillery_stack import DistilleryStack
+from distillery.distillery_tenable import DistilleryTenable
+from distillery.distillery_vultr import DistilleryVultr
 
 app = cdk.App()
+
+DistilleryAmazon(
+    app, 'DistilleryAmazon',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryAzure(
+    app, 'DistilleryAzure',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryCloudflare(
+    app, 'DistilleryCloudflare',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryDigitalOcean(
+    app, 'DistilleryDigitalOcean',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryGoogle(
+    app, 'DistilleryGoogle',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryGoogleCloud(
+    app, 'DistilleryGoogleCloud',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryNetSpi(
+    app, 'DistilleryNetSpi',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryO365(
+    app, 'DistilleryO365',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryOkta(
+    app, 'DistilleryOkta',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryOracle(
+    app, 'DistilleryOracle',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
 
 DistilleryStack(
     app, 'DistilleryStack',
     env = cdk.Environment(
         account = os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region = 'us-west-2'
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryTenable(
+    app, 'DistilleryTenable',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+DistilleryVultr(
+    app, 'DistilleryVultr',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-1'
     ),
     synthesizer = cdk.DefaultStackSynthesizer(
         qualifier = '4n6ir'
---
+++ distillery/distillery_cloudflare.py
@@ -0,0 +1,149 @@
+import cdk_nag
+
+from aws_cdk import (
+    Aspects,
+    Duration,
+    RemovalPolicy,
+    Stack,
+    aws_events as _events,
+    aws_events_targets as _targets,
+    aws_iam as _iam,
+    aws_lambda as _lambda,
+    aws_logs as _logs,
+    aws_logs_destinations as _destinations
+)
+
+from constructs import Construct
+
+class DistilleryCloudflare(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        account = Stack.of(self).account
+        region = Stack.of(self).region
+
+    ### CDK NAG ###
+
+        Aspects.of(self).add(
+            cdk_nag.AwsSolutionsChecks(
+                log_ignores = True,
+                verbose = True
+            )
+        )
+
+        cdk_nag.NagSuppressions.add_stack_suppressions(
+            self, suppressions = [
+                {"id":"AwsSolutions-IAM4","reason":"The IAM user, role, or group uses AWS managed policies."},
+                {"id":"AwsSolutions-IAM5","reason":"The IAM entity contains wildcard permissions and does not have a cdk-nag rule suppression with evidence for those permission."},
+                {"id":"AwsSolutions-L1","reason":"The non-container Lambda function is not configured to use the latest runtime version."},
+                {"id":"AwsSolutions-DDB3","reason":"The DynamoDB table does not have Point-in-time Recovery enabled."},
+            ]
+        )
+
+    ### LAMBDA LAYERS ###
+
+        getpublicip = _lambda.LayerVersion.from_layer_version_arn(
+            self, 'getpublicip',
+            layer_version_arn = 'arn:aws:lambda:'+region+':070176467818:layer:getpublicip:10'
+        )
+
+        requests = _lambda.LayerVersion.from_layer_version_arn(
+            self, 'requests',
+            layer_version_arn = 'arn:aws:lambda:'+region+':070176467818:layer:requests:2'
+        )
+
+    ### ERROR ###
+
+        error = _lambda.Function.from_function_arn(
+            self, 'error',
+            'arn:aws:lambda:'+region+':'+account+':function:shipit-error'
+        )
+
+        timeout = _lambda.Function.from_function_arn(
+            self, 'timeout',
+            'arn:aws:lambda:'+region+':'+account+':function:shipit-timeout'
+        )
+
+    ### IAM ###
+
+        role = _iam.Role(
+            self, 'role',
+            assumed_by = _iam.ServicePrincipal(
+                'lambda.amazonaws.com'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AWSLambdaBasicExecutionRole'
+            )
+        )
+
+        role.add_to_policy(
+            _iam.PolicyStatement(
+                actions = [
+                    's3:PutObject'
+                ],
+                resources = [
+                    'arn:aws:s3:::stage.tundralabs.net/*'
+                ]
+            )
+        )
+
+    ### LAMBDA ###
+
+        compute = _lambda.Function(
+            self, 'compute',
+            runtime = _lambda.Runtime.PYTHON_3_12,
+            code = _lambda.Code.from_asset('sources/cloudflare'),
+            timeout = Duration.seconds(900),
+            handler = 'cloudflare.handler',
+            environment = dict(
+                AWS_ACCOUNT = account,
+                S3_BUCKET = 'stage.tundralabs.net',
+                SOURCE = 'cloudflare'
+            ),
+            memory_size = 512,
+            role = role,
+            layers = [
+                getpublicip,
+                requests
+            ]
+        )
+
+        logs = _logs.LogGroup(
+            self, 'logs',
+            log_group_name = '/aws/lambda/'+compute.function_name,
+            retention = _logs.RetentionDays.ONE_MONTH,
+            removal_policy = RemovalPolicy.DESTROY
+        )
+
+        sub = _logs.SubscriptionFilter(
+            self, 'sub',
+            log_group = logs,
+            destination = _destinations.LambdaDestination(error),
+            filter_pattern = _logs.FilterPattern.all_terms('ERROR')
+        )
+
+        time = _logs.SubscriptionFilter(
+            self, 'time',
+            log_group = logs,
+            destination = _destinations.LambdaDestination(timeout),
+            filter_pattern = _logs.FilterPattern.all_terms('Task','timed','out')
+        )
+
+        event = _events.Rule(
+            self, 'event',
+            schedule = _events.Schedule.cron(
+                minute = '0',
+                hour = '10',
+                month = '*',
+                week_day = '*',
+                year = '*'
+            )
+        )
+
+        event.add_target(
+            _targets.LambdaFunction(compute)
+        )
