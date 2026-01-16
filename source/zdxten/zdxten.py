111f20ec0434148c5d53eff9e6fdba8636849077
John Lukach <hello@lukach.io>
2026-01-16 11:10:20 +0000

    fix

--- source/zdxten/zdxten.py
+++ source/zdxten/zdxten.py
@@ -17,37 +17,16 @@ def handler(event, context):
 
     headers = {'User-Agent': 'Distillery (https://github.com/jblukach/distillery)'}
 
-    future = requests.get('https://config.zscaler.com/api/'+os.environ['SOURCE']+'.net/future/json', headers=headers)
-    print('Future Status Code: '+str(future.status_code))
-
     recommended = requests.get('https://config.zscaler.com/api/'+os.environ['SOURCE']+'.net/hubs/cidr/json/recommended', headers=headers)
     print('Recommended Status Code: '+str(recommended.status_code))
 
     required = requests.get('https://config.zscaler.com/api/'+os.environ['SOURCE']+'.net/hubs/cidr/json/required', headers=headers)
     print('Required Status Code: '+str(required.status_code))
 
-    if future.status_code == 200 and recommended.status_code == 200 and required.status_code == 200:
+    if recommended.status_code == 200 and required.status_code == 200:
 
         f = open('/tmp/'+os.environ['SOURCE']+'.csv', 'w')
         f.write('A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z\n')
-
-        for cidr in future.json()['prefixes']:
-
-            cidr = cidr.strip()
-
-            if ipaddress.ip_network(cidr).version == 4:
-                netrange = ipaddress.IPv4Network(cidr)
-                first, last = netrange[0], netrange[-1]
-                firstip = int(ipaddress.IPv4Address(first))
-                lastip = int(ipaddress.IPv4Address(last))
-                f.write(os.environ['SOURCE']+','+now+','+cidr+','+str(firstip)+','+str(lastip)+',-,future,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-\n')
-
-            if ipaddress.ip_network(cidr).version == 6:
-                netrange = ipaddress.IPv6Network(cidr)
-                first, last = netrange[0], netrange[-1]
-                firstip = int(ipaddress.IPv6Address(first))
-                lastip = int(ipaddress.IPv6Address(last))
-                f.write(os.environ['SOURCE']+','+now+','+cidr+','+str(firstip)+','+str(lastip)+',-,future,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-\n')
 
         for cidr in recommended.json()['hubPrefixes']:
 
